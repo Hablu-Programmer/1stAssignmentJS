1b52dabc221a0a68925546363a4f2deb23cc68ea
mdahadali <habluprogrammer@gmail.com>
2021-08-09 08:52:09 +0600

    1st assignment js

---
+++ assignment.js
@@ -0,0 +1,25 @@
+// ১. Variable কি ?
+
+// ২. Variable কিভাবে লিখতে হয় ?
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+ 
+// asignment.js - GitHub Link 
