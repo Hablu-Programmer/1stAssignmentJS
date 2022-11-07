7fa7038986b6f3901913f5b16dbd593eaca3c585
ujjalmiah023 <71878158+ujjalmiah023@users.noreply.github.com>
2022-11-07 11:07:36 +0600

    Ujjalmiah.js
    আসসালামু আলাইকুম ভাই

--- assignment.js
+++ assignment.js
@@ -1,25 +1,38 @@
 // ১. Variable কি ?
-
+ANS : ভ্যারিয়েবল হলো একটি বক্সের মতো যাতে আমরা ডেটা স্টোর করতেপারে এবং বিভিন্ন কন্ডিশনে ব্যবহার করে প্রোগ্রাম রান করতে পারি.
 // ২. Variable কিভাবে লিখতে হয় ?
-
+ANS: Var ujjal= "MY name";
 // ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
-
-// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
-
+ANS: যে variable এর মান শুধু টেক্সট হিসেবে ব্যবহার করা যায় তাকে string type variable বলে। var my name=ujjal;
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?র্
+ANS: নাম্বার টাইপ variable বলতে যে variable এর মান দিয়ে আমরা যোগ বিযোগ গুন ভাগ করতে পারি  var MYage=20;
 // ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
 
 // ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
-
+ANS: var myaddress= "Dhaka, Narsingdi, Palash, Majerchor, Amtola";
+     console.log(myaddress.toUpperCase());
+     console.log(myaddress.toLowerCase());
 // ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
-
+ANS: add,SUbstracrt, Multiply, Divide, Modulus - division Remainder,Increment,Decrement মোট 7 টি।
 // ৮. Math.abs() এর ব্যাবহার লিখুন । 
-
+ANS:
+var absolute_value= Math.abs(-55.925);
+console.log(absolute_value);
+OUTPUT: 55.925
 // ৯. Math.celi()  এর ব্যাবহার লিখুন । 
-
+ANS:
+console.log(Math.ceil(.95));
+output: 1
 // ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+ANS:
+console.log(Math.floor(5.95));
+output: 5
 
 // ১১. Math.round() এর ব্যাবহার লিখুন ।
-
+ANS: 
+onsole.log(Math.round(-5.05), Math.round(-5.5), Math.round(-5.95));
+output: -5 -5 -6
 // ১২. Math.random() এর ব্যাবহার লিখুন ।
- 
+এইটা প্রেক্টিস কেইরা সেই মজা পাইসি লুডু গেমের মুল চাবিকাঠি এইটা var lodo= Math.random()*6;
+document.write(lodo);
 // asignment.js - GitHub Link 
